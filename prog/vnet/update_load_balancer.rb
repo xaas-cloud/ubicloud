e084163b37a86358c406f17024e2b96cb889b60a
Furkan Sahin <furkan@ubicloud.com>
2024-06-20 14:03:48 +0200

    add port remapping from 80 to 1432

--- prog/vnet/update_load_balancer.rb
+++ prog/vnet/update_load_balancer.rb
@@ -7,7 +7,8 @@ class Prog::Vnet::UpdateLoadBalancer < Prog::Base
     target_vms = vm.load_balancers.first.active_vms
     target_ips_v4 = target_vms.map { _1.nics.first.private_ipv4 }.flatten
     vm_ipv4 = vm.ephemeral_net4.to_s
-    map_text_v4 = target_ips_v4.map.with_index { |ip, i| "#{i} : #{ip}" }.join(", ")
+    map_text_v4 = target_ips_v4.map.with_index { |ip, i| "#{i} : #{ip} . 1432" }.join(", ")
+    neighbors = target_ips_v4.reject { _1.network.to_s == vm.nics.first.private_ipv4.network.to_s }
 
     vm.vm_host.sshable.cmd("sudo ip netns exec #{vm.inhost_name} nft --file -", stdin: <<TEMPLATE)
 table inet lb_table;
@@ -16,17 +17,17 @@ table inet lb_table {
   set target_ips_v4 {
     type ipv4_addr;
     flags interval;
-    elements = { #{target_ips_v4.reject { _1.network.to_s == vm.nics.first.private_ipv4.network.to_s }.join(", ")} }
+    elements = { #{neighbors.join(", ")} }
   }
 
   chain prerouting {
     type nat hook prerouting priority -100; policy accept;
-    ip daddr #{vm_ipv4} tcp dport 80 ct state established,related,new counter dnat to numgen random mod #{target_ips_v4.count} map { #{map_text_v4} }
+    ip daddr #{vm_ipv4} tcp dport 80 ct state established,related,new counter dnat ip to numgen random mod #{target_ips_v4.count} map { #{map_text_v4} }
   }
 
   chain postrouting {
     type nat hook postrouting priority 100; policy accept;
-    ip daddr @target_ips_v4 tcp dport 80 ct state established,related,new counter snat to #{vm.nics.first.private_ipv4.network}
+    ip daddr @target_ips_v4 tcp dport 1432 ct state established,related,new counter snat to #{vm.nics.first.private_ipv4.network}
   }
 }
 TEMPLATE
